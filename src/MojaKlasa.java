0a1b04afedb8bce7113c73dd8bf70f8d51466d3c
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2024-01-11 22:48:51 +0100

    Add first java project

---
+++ src/MojaKlasa.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class MojaKlasa {
+	public static void main(String[] args) {
+		Scanner myVar=new Scanner(System.in);
+		System.out.print(myVar.nextLine());
+		}
+}
